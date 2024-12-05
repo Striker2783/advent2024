dceccd80d24b341a793a2369a0afcc09b2190aa7
Striker2783 <owenxingli@gmail.com>
2024-12-05 09:57:15 -0500

    Changed the run function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,19 @@
 use std::{path::Path, process::exit};
 
-pub mod one;
-pub mod two;
-pub mod three;
-pub mod four;
 pub mod five;
+pub mod four;
+pub mod one;
+pub mod three;
+pub mod two;
+
+const FNS: &[fn(&Path)] = &[one::run, two::run, three::run, four::run, five::run];
 
 pub fn run(n: u32, f: &Path) {
-    match n {
-        1 => one::run(f),
-        2 => two::run(f),
-        3 => three::run(f),
-        4 => four::run(f),
-        5 => five::run(f),
-        _ => {
-            eprintln!("Invalid day");
-            exit(0);
-        }
+    let i = (n - 1) as usize;
+    if i >= FNS.len() {
+        eprintln!("Invalid day");
+        exit(0);
+    } else {
+        FNS[i](f);
     }
 }
