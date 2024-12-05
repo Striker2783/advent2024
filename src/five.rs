83d0e9513afe363e995e331a6e2cb0852314f4c5
Striker2783 <owenxingli@gmail.com>
2024-12-05 08:53:22 -0500

    Day five WIP

---
+++ src/five.rs
@@ -0,0 +1,114 @@
+use std::{
+    collections::{HashMap, HashSet},
+    fs,
+    path::Path,
+};
+
+pub fn run(f: &Path) {
+    let s = fs::read_to_string(f).unwrap();
+    println!("{}", Solution::solve_one(&s));
+    println!("{}", Solution::solve_two(&s));
+}
+
+struct Solution {
+    map: HashMap<u32, Vec<u32>>,
+    inverse: HashMap<u32, Vec<u32>>,
+    updates: Vec<Vec<u32>>,
+}
+
+impl Solution {
+    fn solve_one(s: &str) -> u32 {
+        let solution = Solution::from(s);
+        let mut sum = 0;
+        'outer: for v in &solution.updates {
+            let mut wrong = HashSet::new();
+            for &n in v {
+                if wrong.contains(&n) {
+                    continue 'outer;
+                }
+                if let Some(v) = solution.map.get(&n) {
+                    for &n in v {
+                        wrong.insert(n);
+                    }
+                }
+            }
+            sum += v[v.len() / 2];
+        }
+        sum
+    }
+    fn solve_two(s: &str) -> u32 {
+        let solution = Solution::from(s);
+        for v_ori in &solution.updates {
+            let mut v= v_ori.clone();
+            loop {
+                let mut wrong = HashSet::new();
+                let mut fix_needed = HashSet::new();
+                for &n in &v {
+                    if wrong.contains(&n) {
+                        fix_needed.insert(n);
+                    }
+                    if let Some(v) = solution.map.get(&n) {
+                        for &n in v {
+                            wrong.insert(n);
+                        }
+                    }
+                }
+                if fix_needed.is_empty() {
+                    break;
+                }
+                println!("{fix_needed:?}");
+                println!("{v:?}");
+            }
+            if v != *v_ori {
+                continue;
+            }
+        }
+        todo!()
+    }
+}
+impl From<&str> for Solution {
+    fn from(value: &str) -> Self {
+        let mut split = value.split("\n\n");
+        let rules = split.next().unwrap();
+        let updates = split.next().unwrap();
+        let mut map: HashMap<u32, Vec<u32>> = HashMap::new();
+        let mut inverse: HashMap<u32, Vec<u32>> = HashMap::new();
+        rules.lines().for_each(|s| {
+            let mut split = s.split("|");
+            let dependency = split.next().unwrap().parse::<u32>().unwrap();
+            let dependent = split.next().unwrap().parse::<u32>().unwrap();
+            map.entry(dependent)
+                .and_modify(|v: &mut Vec<u32>| v.push(dependency))
+                .or_insert(vec![dependency]);
+            inverse.entry(dependency)
+                .and_modify(|v: &mut Vec<u32>| v.push(dependent))
+                .or_insert(vec![dependent]);
+        });
+        Solution {
+            map,
+            updates: updates
+                .lines()
+                .map(|line| {
+                    line.split(',')
+                        .map(|s| s.parse::<u32>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect(),
+            inverse,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::five::Solution;
+
+    #[test]
+    fn test_solve_one() {
+        assert_eq!(Solution::solve_one("47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47\n"),143);
+    }
+    #[test]
+    fn test_solve_two() {
+        assert_eq!(Solution::solve_two("47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47\n"),123);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod one;
 pub mod two;
 pub mod three;
 pub mod four;
+pub mod five;
 
 pub fn run(n: u32, f: &Path) {
     match n {
@@ -11,6 +12,7 @@ pub fn run(n: u32, f: &Path) {
         2 => two::run(f),
         3 => three::run(f),
         4 => four::run(f),
+        5 => five::run(f),
         _ => {
             eprintln!("Invalid day");
             exit(0);
